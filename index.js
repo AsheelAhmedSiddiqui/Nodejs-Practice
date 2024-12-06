e3439539da2f31eee71eac059ec1386b001722fe
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-12-06 23:53:58 +0500

    post put add delete request

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-const middleware = (req, res, next) => {};
+const middleware = (req, res, next) => {
+	// next();
+};
 
-app.get("/", middleware, (req, res) => {
+// app.use(middleware);
+
+app.get("/", (req, res) => {
 	console.log("req.accepted=>");
 	res.send("Hello World");
 });
 
+app.put("/", (req, res) => {
+	console.log("put ki request");
+});
+
+app.delete("/", (req, res) => {
+	console.log("delete ki request");
+});
+
+app.post("/", (req, res) => {
+	console.log("post ki request");
+});
+
 app.listen(PORT, () => console.log(`Server is runing on PORT ${PORT}`));
-
-// const server = createServer((req, res) => {
-// 	res.statusCode = 200;
-// 	res.setHeader("Content-Type", "text/plain");
-// 	res.end("Hello World");
-// });
-
-// server.listen(port, hostname, () => {
-// 	console.log(`Server running at http://${hostname}:${port}/`);
-// });
